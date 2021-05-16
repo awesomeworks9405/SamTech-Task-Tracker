7cb1e5a1805964ffb6c6c15f4d8d0515ccfe2b1a
Samuel Uche <samueluche8@gmail.com>
2021-05-16 22:53:23 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState(
     [
@@ -27,20 +30,35 @@ function App() {
     ]
 )
 
+
+// Add Task
+const addTask = (task) => {
+const id = Math.floor(Math.random() * 1000) + 1
+
+const newTask = { id, ...task}
+
+setTasks([...tasks, newTask])
+}
+
 //  Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
-// Toggle Reminder
-
+// Toggle Reminder - changes the value of the reminder to the !(opposite) of the reminder
 const toggleReminder = (id) => {
-  console.log(id);
+  setTasks(tasks.map((task) => 
+  task.id === id ? {...task, reminder:
+  !task.reminder} : task )
+  )
 }
+
+// if(&&) ShowAddTask is true perform ... no need for :(else)
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : 'No Task to Display'}
     </div>
   );
